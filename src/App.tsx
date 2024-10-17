ae267f90c52675199e01277e2e5cec50f8fb181e
AbdallahAbozeid <abdallahabozeid10@gmail.com>
2024-10-17 20:52:17 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   useEffect(() => {
     async function fetchAPIData() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+      console.log(NASA_KEY)
       const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
 
 
